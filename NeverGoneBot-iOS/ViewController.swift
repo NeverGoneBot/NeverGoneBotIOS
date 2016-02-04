5e9f30b9717e321da26ad1c4a54e01467fa6f01d
aadesh <aadeshp95@gmail.com>
2016-02-03 20:27:18 -0500

    Initial Commit

---
+++ NeverGoneBot-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeverGoneBot-iOS
+//
+//  Created by Aadesh Patel on 2/3/16.
+//  Copyright © 2016 Aadesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
